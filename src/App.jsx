2c79d8ca1415ad19f97c950cd1ff325ff2392a90
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-11-03 18:58:18 -0400

    Add 404 not found page to page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout'
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="home" />} />
         <Route path="home" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
   )
