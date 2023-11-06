4cd606e507e58f2ef6363c840eed96abb55479bd
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-11-06 14:14:00 -0500

    Add the academics button page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout'
 import Home from './pages/Home';
+import Academics from './pages/Academics';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="home" />} />
         <Route path="home" element={<Home />} />
+        <Route path="academics" element={<Academics />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
