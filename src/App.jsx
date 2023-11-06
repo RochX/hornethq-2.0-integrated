6551fe2ac43fb24573a2db5ee42d3ec28eb0ad7f
mpukabaofficial <99944714+mpukabaofficial@users.noreply.github.com>
2023-11-06 16:07:21 -0500

    carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Layout from './Layout'
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import { Routes, Route, Navigate } from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage/IndexPage";
+import NotFound from "./pages/NotFound";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="home" />} />
-        <Route path="home" element={<Home />} />
+        <Route path="/home" element={<Home />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-  )
+  );
 }
 
 export default App;
