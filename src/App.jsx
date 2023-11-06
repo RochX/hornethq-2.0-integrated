5c42e5ae16ae80afcf43055598b342b8f38988b3
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-11-06 14:16:09 -0500

    Add basic page for grades

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Layout from './Layout'
 import Home from './pages/Home';
 import Academics from './pages/Academics';
+import Grades from './pages/Academics/Grades';
 import NotFound from './pages/NotFound';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route index element={<Navigate to="home" />} />
         <Route path="home" element={<Home />} />
         <Route path="academics" element={<Academics />} />
+        <Route path="academics/grades" element={<Grades />} />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
