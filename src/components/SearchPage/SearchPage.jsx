463b7f6e8b08ac1f87898add84c1469220660ffa
amscorsone <amalia.scorsone21@kzoo.edu>
2023-11-08 15:45:18 -0500

    changed the file names

---
+++ src/components/SearchPage/SearchPage.jsx
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import React from 'react';
+import './SearchPage.css';
+import DropdownMenu from './DropdownMenu';
+import SubjectDropdown from './SubjectDropdown';
+import Navbar from './NavBar';
+import './NavBar.css';
+import Modal from './Modal'
+import ClassPicker from './ClassPicker'
+import { useState } from 'react';
+
+
+function App() {
+    const [openModal, setOpenModal] = useState(false);
+  return (
+    <div className="App">
+      <h1>Hornet HQ</h1>
+      <Navbar/>
+      <h3>click the button to see your schedule</h3>
+      <button className="openModalButton" 
+      onClick={() => {
+        setOpenModal(true);
+      }}
+      >
+        open
+       </button>
+       {openModal && <Modal closeModal={setOpenModal} />}
+      <DropdownMenu />
+      <SubjectDropdown />
+      <ClassPicker/>
+
+
+    </div>
+  );
+}
+
+export default App;
