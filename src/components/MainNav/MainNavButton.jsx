7805eaf400a2a5744edfbe46110da7982091048e
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-11-10 13:32:21 -0500

    Dynamically set whether the main nav buttons are one or two columns
    Currently only updates on page refresh

--- src/components/MainNav/MainNavButton.jsx
+++ src/components/MainNav/MainNavButton.jsx
@@ -5,14 +5,28 @@ import { Link } from "react-router-dom";
 // It also has an icon within a grey circle. Create fallback text in case image is not present
 
 function MainNavButton (props) {
+  const column_format_selected = getColumnFormat()
+  console.log(innerWidth)
+
+  const main_nav_classname = "main-nav-button " + column_format_selected
+
   return (
-    <div className="main-nav-button" id={props.id}>
+    <div className={main_nav_classname}  id={props.id}>
       <Link to={props.path}>
         <h3>{props.title}</h3>
         <p>{props.description}</p>
       </Link>
     </div>
   );
+}
+
+function getColumnFormat() {
+  if (innerWidth < 900) {
+    return "one-col"
+  }
+  else {
+    return "two-col"
+  }
 }
 
 export default MainNavButton;
