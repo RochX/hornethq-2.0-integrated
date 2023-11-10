81d1cc5a8554850b1df46053c7b539918ba0ea27
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-11-10 13:48:07 -0500

    Change from one column to two column format when page is resized
    Now the main navigation updates fully dynamically

--- src/components/MainNav/MainNavButton.jsx
+++ src/components/MainNav/MainNavButton.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useWindowDimensions from "../../helperFunctions/windowDimensions";
 // PageButton should mimic the current Hornet HQ 2.0 button on the home page
 // It has a title and description on what it where it goes
 // It also has an icon within a grey circle. Create fallback text in case image is not present
 
 function MainNavButton (props) {
   const column_format_selected = getColumnFormat()
-  console.log(innerWidth)
 
   const main_nav_classname = "main-nav-button " + column_format_selected
 
@@ -21,7 +21,8 @@ function MainNavButton (props) {
 }
 
 function getColumnFormat() {
-  if (innerWidth < 900) {
+  const windowDimensions = useWindowDimensions();
+  if (windowDimensions.width < 900) {
     return "one-col"
   }
   else {
