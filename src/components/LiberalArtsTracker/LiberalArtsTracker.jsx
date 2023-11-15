ec91b5467307af98b8df53072017e7bfb20adb1a
Xavier Silva <Xavier.Silva20@kzoo.edu>
2023-11-15 13:49:49 -0500

    Move styling of progress bars into its own function

--- src/components/LiberalArtsTracker/LiberalArtsTracker.jsx
+++ src/components/LiberalArtsTracker/LiberalArtsTracker.jsx
@@ -1,7 +1,7 @@
 import ProgressBar from "../ProgressBar/ProgressBar";
 import "./LiberalArtsTracker.css";
 
-const progess_bars = [
+const lib_arts_progess_bars = [
   <ProgressBar bgcolor="red" completed={50} title="STEM" />,
   <ProgressBar bgcolor="#44ADA5" completed={20} title="Social Sciences" />,
   <ProgressBar bgcolor="blue" completed={50} title="Humanities" />,
@@ -10,28 +10,32 @@ const progess_bars = [
 ]
 
 function LiberalArtsTracker() {
-  const pbar_width = 100/progess_bars.length;
-  const pbar_styling = {
-    width: `${pbar_width}%`
-  }
-
-  const styled_progress_bars = progess_bars.map((pbar, index) => {
-     const pbar_classname = "pbar_" + index;
-     return (
-      <div className={pbar_classname} style={pbar_styling}>
-        {pbar}
-      </div>
-     );
-  });
-
   return (
     <div className="lib-art-tracker">
       <ProgressBar bgcolor="#EA6820" completed={40} title="Graduation Progress" />
       <div className="lib-art-tracker-subset">
-        {styled_progress_bars}
+        {style_progress_bars(lib_arts_progess_bars)}
       </div>
     </div>
   )
+}
+
+function style_progress_bars(progress_bars) {
+  const pbar_width = 100/progress_bars.length;
+  const pbar_styling = {
+    width: `${pbar_width}%`
+  }
+
+  const styled_progress_bars = progress_bars.map((pbar, index) => {
+    const pbar_id = "pbar_" + index;
+    return (
+     <div className="styled-pbar" style={pbar_styling} id={pbar_id} key={pbar_id}>
+       {pbar}
+     </div>
+    );
+ });
+
+ return styled_progress_bars;
 }
 
 export default LiberalArtsTracker;
