4e84d8660b8b82c028a76afdc5a87e79479f9f34
Harsha Ajjarapu <Harsha.ajjarapu@gmail.com>
2023-11-15 10:00:17 -0500

    Update Calendar.jsx
    Carousel now only displays 2023 (-2024) winter and spring classes!
    Also updated the faculty name appearing multiple times.
    
    Calendar now shows the name of the student who is logged in.

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -14,6 +14,7 @@ import {
 
 const Calendar = () => {
   const [schedule, setSchedule] = useState([]);
+  const [studentName, setStudentName] = useState(""); // State to store student's name
 
   useEffect(() => {
     const fetchData = async () => {
@@ -24,17 +25,20 @@ const Calendar = () => {
           return;
         }
 
-        const offeringsResponse = await axios.get(
-          "https://hhqv2backend.vercel.app/api/offering"
-        );
         const studentResponse = await axios.get(
           "https://hhqv2backend.vercel.app/api/student"
         );
+        const offeringsResponse = await axios.get(
+          "https://hhqv2backend.vercel.app/api/offering"
+        );
+
         const studentData = studentResponse.data.find(
           (student) => student.student_id === studentId
         );
 
         if (studentData) {
+          setStudentName(studentData.name); // Set the student's name for the welcome message
+
           const enrolledCourses = studentData.enrollment
             .filter((enrollment) => enrollment.grade === "In Progress")
             .map((enrollment) => {
@@ -46,8 +50,8 @@ const Calendar = () => {
                     time: `${offeringDetails.start_time} - ${offeringDetails.end_time}`,
                     event: offeringDetails.name,
                     status: "In Progress",
-                    Building: offeringDetails.building || "TBD",
-                    classNum: offeringDetails.room || "TBD",
+                    building: offeringDetails.building || "TBD",
+                    room: offeringDetails.room || "TBD",
                   }
                 : null;
             })
@@ -71,8 +75,13 @@ const Calendar = () => {
         <Box display="flex" justifyContent="center" alignItems="center" mb={2}>
           <Clock />
         </Box>
+        {studentName && (
+          <Typography variant="h5" gutterBottom>
+            Welcome, {studentName}!
+          </Typography>
+        )}
         <Typography variant="h4" gutterBottom>
-          My Schedule
+          Schedule
         </Typography>
         <List>
           {schedule.map((item, index) => (
@@ -83,7 +92,7 @@ const Calendar = () => {
                   secondary={`${item.status} | ${item.time}`}
                 />
                 <Typography variant="body2" color="textSecondary">
-                  {`${item.Building} ${item.classNum}`}
+                  {`${item.building} ${item.room}`}
                 </Typography>
               </ListItem>
               {index < schedule.length - 1 && <Divider />}
